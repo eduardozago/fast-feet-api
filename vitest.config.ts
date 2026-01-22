5677caa6fcf84d064ff001382851711e5d46e696
Eduardo Zago <zagoe22@gmail.com>
2026-01-22 11:05:05 -0300

    chore: add test path alias to TypeScript and Vitest configurations

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
     alias: {
       // Ensure Vitest correctly resolves TypeScript path aliases
       '@/': `${resolve(__dirname, './src')}/`,
+      'test/': `${resolve(__dirname, './test')}/`,
     },
   },
 })
