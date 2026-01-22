8f79cde6942e90f3882b5d50293de23c23532df0
Eduardo Zago <zagoe22@gmail.com>
2026-01-22 13:17:43 -0300

    feat: add AuthenticateUseCase for user login with email/password validation and access token generation

---
+++ src/domain/identity/application/use-cases/authenticate.ts
@@ -0,0 +1,53 @@
+import { AccountsRepository } from '../repositories/accounts-repository'
+import { Either, left, right } from '@/core/either'
+import { HashChecker } from '../cryptography/hash-checker'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { Encrypter } from '../cryptography/encrypter'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+export type AuthenticateUseCaseResponse = Either<
+  InvalidCredentialsError,
+  {
+    accessToken: string
+  }
+>
+
+export class AuthenticateUseCase {
+  constructor(
+    private accountsRepository: AccountsRepository,
+    private hashChecker: HashChecker,
+    private encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const account = await this.accountsRepository.findByEmail(email)
+
+    if (!account) {
+      return left(new InvalidCredentialsError())
+    }
+
+    const isPasswordValid = await this.hashChecker.check(
+      password,
+      account.password,
+    )
+
+    if (!isPasswordValid) {
+      return left(new InvalidCredentialsError())
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: account.id.toString(),
+    })
+
+    return right({
+      accessToken,
+    })
+  }
+}
