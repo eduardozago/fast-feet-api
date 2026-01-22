980cd579d2ba81b2c73b49917307610bbeaea909
Eduardo Zago <zagoe22@gmail.com>
2026-01-22 11:20:09 -0300

    test: add InMemoryAccountsRepository for identity domain testing with findById, findByEmail, and create methods

---
+++ test/repositories/identity/in-memory-accounts-repository.ts
@@ -0,0 +1,32 @@
+import { AccountsRepository } from '@/domain/identity/application/repositories/accounts-repository'
+import { Account } from '@/domain/identity/enterprise/entities/account'
+
+export class InMemoryAccountsRepository implements AccountsRepository {
+  public items: Account[] = []
+
+  findById(id: string): Promise<Account | null> {
+    const account = this.items.find((item) => item.id.toString() === id)
+
+    if (!account) {
+      return Promise.resolve(null)
+    }
+
+    return Promise.resolve(account)
+  }
+
+  findByEmail(email: string): Promise<Account | null> {
+    const account = this.items.find((item) => item.email === email)
+
+    if (!account) {
+      return Promise.resolve(null)
+    }
+
+    return Promise.resolve(account)
+  }
+
+  create(account: Account): Promise<void> {
+    this.items.push(account)
+
+    return Promise.resolve()
+  }
+}
