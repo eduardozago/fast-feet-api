898f7336683662917f96698b0f232a386002199b
Eduardo Zago <zagoe22@gmail.com>
2026-01-23 17:18:58 -0300

    feat: add Argon2HashGenerator implementation for password hashing with argon2id algorithm and custom memory/time cost configuration

---
+++ src/infra/cryptography/argon2-hash-generator.ts
@@ -0,0 +1,17 @@
+import { HashGenerator } from '@/domain/identity/application/cryptography/hash-generator'
+import * as argon2 from 'argon2'
+
+export class Argon2HashGenerator implements HashGenerator {
+  private options: argon2.Options = {
+    type: argon2.argon2id,
+    memoryCost: 47104,
+    timeCost: 3,
+    parallelism: 2,
+  }
+
+  async hash(plainText: string): Promise<string> {
+    const passwordHashed = argon2.hash(plainText, this.options)
+
+    return passwordHashed
+  }
+}
