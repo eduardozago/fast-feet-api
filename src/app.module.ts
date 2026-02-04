382829cbb7420c0897729fd99957defd318efb22
Eduardo Zago <zagoe22@gmail.com>
2026-02-04 09:33:53 -0300

    feat: add EnvModule, HttpModule, and AuthModule imports with environment schema validation in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { DatabaseModule } from './infra/database/database.module'
+import { envSchema } from './infra/env/env'
+import { EnvModule } from './infra/env/env.module'
+import { HttpModule } from './infra/http/http.module'
+import { AuthModule } from './infra/auth/auth.module'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    EnvModule,
+    HttpModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
