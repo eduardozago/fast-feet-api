631f7dd52390c48f179b1fc249792a1ceebd5bf9
Eduardo Zago <zagoe22@gmail.com>
2026-02-04 09:33:04 -0300

    test: add e2e test for POST /sessions endpoint with AccountFactory, password hashing, and JWT access token validation

---
+++ src/infra/http/controllers/identity/authenticate.controller.e2e-spec.ts
@@ -0,0 +1,49 @@
+import { AppModule } from '@/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import * as argon2 from 'argon2'
+import request from 'supertest'
+import { AccountFactory } from 'test/factories/identity/make-account'
+import { PrismaServiceE2E } from 'test/prisma-service-e2e'
+
+describe('Authenticate (E2E)', () => {
+  let app: INestApplication
+  let accountFactory: AccountFactory
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [AccountFactory],
+    })
+      .overrideProvider(PrismaService)
+      .useClass(PrismaServiceE2E)
+      .compile()
+
+    app = moduleRef.createNestApplication()
+
+    accountFactory = moduleRef.get(AccountFactory)
+
+    await app.init()
+  })
+
+  test('[POST] /sessions', async () => {
+    await accountFactory.makePrismaAccount({
+      email: 'johndoe@example.com',
+      password: await argon2.hash('12345678'),
+    })
+
+    const response = await request(app.getHttpServer()).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '12345678',
+    })
+
+    expect(response.statusCode).toBe(201)
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        access_token: expect.any(String),
+      }),
+    )
+  })
+})
