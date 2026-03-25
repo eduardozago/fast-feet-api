e09d8072e8e4d9078da44640612ce3bf24d727f6
Eduardo Zago <zagoe22@gmail.com>
2026-03-25 19:59:25 -0300

    test: add e2e test for CreateRecipientAddressController
    Add e2e test suite for POST /recipients/addresses endpoint with ADMIN authentication. Test verifies creating recipient address with all address fields (street, number, complement, neighborhood, city, state, country, postalCode). Validate response status 201 and recipient address persisted in database with correct properties. Add RecipientFactory with makePrismaRecipient method for test recipient creation and use PrismaServiceE2E for database operations

--- test/factories/delivery/make-recipient.ts
+++ test/factories/delivery/make-recipient.ts
@@ -5,6 +5,9 @@ import {
   RecipientProps,
 } from '@/domain/delivery/enterprise/entities/recipient'
 import { makeIdentityDocument } from './make-identity-type'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaRecipientMapper } from '@/infra/database/prisma/mappers/delivery/prisma-recipient-mapper'
 
 export function makeRecipient(
   override: Partial<RecipientProps> = {},
@@ -20,4 +23,21 @@ export function makeRecipient(
   )
 
   return recipient
+}
+
+@Injectable()
+export class RecipientFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaRecipient(
+    data: Partial<RecipientProps> = {},
+  ): Promise<Recipient> {
+    const recipient = makeRecipient(data)
+
+    await this.prisma.recipient.create({
+      data: PrismaRecipientMapper.toPrisma(recipient),
+    })
+
+    return recipient
+  }
 }
