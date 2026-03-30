35be1baf4968a589e8b703090aac8df30a5e512c
Eduardo Zago <zagoe22@gmail.com>
2026-03-29 22:44:04 -0300

    feat: add update method to DeliveriesRepository interface
    Add abstract update method to DeliveriesRepository for updating delivery entities

--- src/domain/delivery/application/repositories/deliveries-repository.ts
+++ src/domain/delivery/application/repositories/deliveries-repository.ts
@@ -3,4 +3,5 @@ import { Delivery } from '../../enterprise/entities/delivery'
 export abstract class DeliveriesRepository {
   abstract findById(id: string): Promise<Delivery | null>
   abstract create(delivery: Delivery): Promise<void>
+  abstract update(delivery: Delivery): Promise<void>
 }
