95bf45f870d736de0a65d4c0b02a6b45b1d3e305
Eduardo Zago <zagoe22@gmail.com>
2026-04-05 22:42:13 -0300

    test: add E2E test for WaitForPickupController PATCH endpoint
    Add E2E test verifying delivery status transition from CREATED to WAITING_PICKUP. Create admin account with ADMIN role and generate JWT access token. Create recipient, recipient address and delivery with CREATED status using factories. Send PATCH request to /deliveries/:id/wait-for-pickup with authorization header and verify 204 status code. Query database to confirm delivery status was updated to WAITING_PICKUP. Add DeliveryFactory with makePrismaDelivery method to create

--- test/factories/delivery/make-delivery.ts
+++ test/factories/delivery/make-delivery.ts
@@ -2,7 +2,11 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Delivery,
   DeliveryProps,
+  DeliveryStatus,
 } from '@/domain/delivery/enterprise/entities/delivery'
+import { PrismaDeliveryMapper } from '@/infra/database/prisma/mappers/delivery/prisma-delivery-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
 
 export function makeDelivery(
   override: Partial<DeliveryProps> = {},
@@ -12,10 +16,28 @@ export function makeDelivery(
     {
       recipientId: new UniqueEntityID(),
       recipientAddressId: new UniqueEntityID(),
+      status: DeliveryStatus.CREATED,
       ...override,
     },
     id,
   )
 
   return delivery
+}
+
+@Injectable()
+export class DeliveryFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaDelivery(
+    data: Partial<DeliveryProps> = {},
+  ): Promise<Delivery> {
+    const delivery = makeDelivery(data)
+
+    await this.prisma.delivery.create({
+      data: PrismaDeliveryMapper.toPrisma(delivery),
+    })
+
+    return delivery
+  }
 }
