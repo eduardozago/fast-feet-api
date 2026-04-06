7c93d50dd8aca7d6d0470f091b9a61e6beb5303d
Eduardo Zago <zagoe22@gmail.com>
2026-04-06 19:54:05 -0300

    test: add E2E test for StartTransitController PATCH endpoint
    Add E2E test verifying delivery status transition from WAITING_PICKUP to IN_TRANSIT. Create admin account with ADMIN role and generate JWT access token. Create recipient, recipient address, delivery with WAITING_PICKUP status and courier using factories. Send PATCH request to /deliveries/:id/start-transit with courierId in request body and authorization header and verify 204 status code. Query database to confirm delivery status was updated to IN_TRANSIT.

--- test/factories/delivery/make-courier.ts
+++ test/factories/delivery/make-courier.ts
@@ -3,6 +3,9 @@ import {
   Courier,
   CourierProps,
 } from '@/domain/delivery/enterprise/entities/courier'
+import { PrismaCourierMapper } from '@/infra/database/prisma/mappers/delivery/prisma-courier-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
 
 export function makeCourier(
   override: Partial<CourierProps> = {},
@@ -18,4 +21,19 @@ export function makeCourier(
   )
 
   return courier
+}
+
+@Injectable()
+export class CourierFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaCourier(data: Partial<CourierProps> = {}): Promise<Courier> {
+    const courier = makeCourier(data)
+
+    await this.prisma.courier.create({
+      data: PrismaCourierMapper.toPrisma(courier),
+    })
+
+    return courier
+  }
 }
