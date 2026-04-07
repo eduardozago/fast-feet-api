e26ccfa5dd17a2203c7577c3326106ca0bd6c3df
Eduardo Zago <zagoe22@gmail.com>
2026-04-07 19:32:03 -0300

    test: add unit test for delete delivery status validation
    Add test case verifying CannotDeleteDeliveryError is returned when attempting to delete delivery with IN_TRANSIT status. Update existing test to explicitly set delivery status to CREATED. Import DeliveryStatus enum and CannotDeleteDeliveryError

--- src/domain/delivery/application/use-cases/delivery/delete-delivery.spec.ts
+++ src/domain/delivery/application/use-cases/delivery/delete-delivery.spec.ts
@@ -1,6 +1,8 @@
 import { InMemoryDeliveriesRepository } from 'test/repositories/delivery/in-memory-deliveries-repository'
 import { makeDelivery } from 'test/factories/delivery/make-delivery'
 import { DeleteDeliveryUseCase } from './delete-delivery'
+import { DeliveryStatus } from '@/domain/delivery/enterprise/entities/delivery'
+import { CannotDeleteDeliveryError } from './errors/cannot-delete-delivery-error'
 
 let deliveriesRepository: InMemoryDeliveriesRepository
 let sut: DeleteDeliveryUseCase
@@ -12,7 +14,9 @@ describe('Delete Delivery', () => {
   })
 
   it('should be able to delete delivery', async () => {
-    const delivery = makeDelivery()
+    const delivery = makeDelivery({
+      status: DeliveryStatus.CREATED,
+    })
 
     await deliveriesRepository.create(delivery)
 
@@ -22,5 +26,20 @@ describe('Delete Delivery', () => {
 
     expect(result.isRight()).toBe(true)
     expect(deliveriesRepository.items.length).toBe(0)
+  })
+
+  it('should be not able to delete delivery with status other than CREATED', async () => {
+    const delivery = makeDelivery({
+      status: DeliveryStatus.IN_TRANSIT,
+    })
+
+    await deliveriesRepository.create(delivery)
+
+    const result = await sut.execute({
+      deliveryId: delivery.id.toString(),
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(CannotDeleteDeliveryError)
   })
 })
