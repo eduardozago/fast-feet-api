d86acd9cd978e237f8612ee1b6dc63158eccf7a1
Eduardo Zago <zagoe22@gmail.com>
2026-04-10 19:58:03 -0300

    test: add test case for geocoding service failure in CreateRecipientAddressUseCase
    Add FakeGeocodingService to CreateRecipientAddressUseCase test setup and verify that recipient address creation fails when geocoding service returns error. Set shouldFail flag to true and assert that result is left with GeocodingServiceError and no address is created in repository

--- src/domain/delivery/application/use-cases/recipient/create-recipient-address.spec.ts
+++ src/domain/delivery/application/use-cases/recipient/create-recipient-address.spec.ts
@@ -3,18 +3,23 @@ import { makeRecipient } from 'test/factories/delivery/make-recipient'
 import { InMemoryRecipientAddressesRepository } from 'test/repositories/delivery/in-memory-recipient-addresses-repository'
 import { CreateRecipientAddressUseCase } from './create-recipient-address'
 import { RecipientNotFoundError } from './errors/recipient-not-found-error'
+import { FakeGeocodingService } from 'test/location/fake-geocoding-service'
+import { GeocodingServiceError } from '../errors/geocoding-service-error'
 
 let recipientAdressesRepository: InMemoryRecipientAddressesRepository
 let recipientsRepository: InMemoryRecipientsRepository
+let geocodingService: FakeGeocodingService
 let sut: CreateRecipientAddressUseCase
 
 describe('Create Recipient Address', () => {
   beforeEach(() => {
     recipientAdressesRepository = new InMemoryRecipientAddressesRepository()
     recipientsRepository = new InMemoryRecipientsRepository()
+    geocodingService = new FakeGeocodingService()
     sut = new CreateRecipientAddressUseCase(
       recipientAdressesRepository,
       recipientsRepository,
+      geocodingService,
     )
   })
 
@@ -67,5 +72,29 @@ describe('Create Recipient Address', () => {
     expect(result.isLeft()).toBe(true)
     expect(result.value).toBeInstanceOf(RecipientNotFoundError)
     expect(recipientAdressesRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to create a recipient address if geocoding fails', async () => {
+    const recipient = makeRecipient()
+    await recipientsRepository.create(recipient)
+
+    // Force geocoding to fail
+    geocodingService.shouldFail = true
+
+    const result = await sut.execute({
+      recipientId: recipient.id.toString(),
+      street: 'Street 1',
+      number: '123',
+      complement: 'Complement 1',
+      neighborhood: 'Neighborhood 1',
+      city: 'City 1',
+      state: 'State 1',
+      postalCode: '12345-678',
+      country: 'Country 1',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(recipientAdressesRepository.items).toHaveLength(0)
+    expect(result.value).toBeInstanceOf(GeocodingServiceError)
   })
 })
