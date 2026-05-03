f0bec0397cad82fafeafa2096d1424b82f255b1f
Eduardo Zago <zagoe22@gmail.com>
2026-05-02 21:15:33 -0300

    feat: add optional status filter to FetchDeliveriesUseCase
    Add DeliveryStatus import, include optional status parameter in FetchDeliveriesUseCaseRequest interface, and pass status filter to deliveriesRepository.findMany method call

--- src/domain/delivery/application/use-cases/delivery/fetch-deliveries.ts
+++ src/domain/delivery/application/use-cases/delivery/fetch-deliveries.ts
@@ -1,9 +1,13 @@
 import { Either, right } from '@/core/either'
 import { Injectable } from '@nestjs/common'
 import { DeliveriesRepository } from '../../repositories/deliveries-repository'
-import { Delivery } from '@/domain/delivery/enterprise/entities/delivery'
+import {
+  Delivery,
+  DeliveryStatus,
+} from '@/domain/delivery/enterprise/entities/delivery'
 
 interface FetchDeliveriesUseCaseRequest {
+  status?: DeliveryStatus
   page: number
   limit: number
 }
@@ -20,13 +24,19 @@ export class FetchDeliveriesUseCase {
   constructor(private deliveriesRepository: DeliveriesRepository) {}
 
   async execute({
+    status,
     page,
     limit,
   }: FetchDeliveriesUseCaseRequest): Promise<FetchDeliveriesUseCaseResponse> {
-    const deliveries = await this.deliveriesRepository.findMany({
-      page,
-      limit,
-    })
+    const deliveries = await this.deliveriesRepository.findMany(
+      {
+        page,
+        limit,
+      },
+      {
+        status,
+      },
+    )
 
     return right({
       deliveries,
