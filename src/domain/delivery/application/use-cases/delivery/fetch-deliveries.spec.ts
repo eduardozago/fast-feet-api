f9a60381d7aa1841ff660ee665fd640edb468a5c
Eduardo Zago <zagoe22@gmail.com>
2026-05-03 21:07:12 -0300

    test: update FetchDeliveriesUseCase tests to use InMemoryRecipientsRepository and InMemoryCouriersRepository
    Add InMemoryRecipientsRepository and InMemoryCouriersRepository as dependencies for InMemoryDeliveriesRepository. Create recipient and courier entities for all test deliveries and associate them via recipientId and courierId. Update test setup to initialize repositories in correct order and pass them to deliveries repository constructor

--- src/domain/delivery/application/use-cases/delivery/fetch-deliveries.spec.ts
+++ src/domain/delivery/application/use-cases/delivery/fetch-deliveries.spec.ts
@@ -2,19 +2,44 @@ import { FetchDeliveriesUseCase } from './fetch-deliveries'
 import { InMemoryDeliveriesRepository } from 'test/repositories/delivery/in-memory-deliveries-repository'
 import { makeDelivery } from 'test/factories/delivery/make-delivery'
 import { DeliveryStatus } from '@/domain/delivery/enterprise/entities/delivery'
+import { InMemoryRecipientsRepository } from 'test/repositories/delivery/in-memory-recipients-repository'
+import { InMemoryCouriersRepository } from 'test/repositories/delivery/in-memory-couriers-repository'
+import { makeRecipient } from 'test/factories/delivery/make-recipient'
+import { makeCourier } from 'test/factories/delivery/make-courier'
 
 let deliveriesRepository: InMemoryDeliveriesRepository
+let recipientsRepository: InMemoryRecipientsRepository
+let couriersRepository: InMemoryCouriersRepository
 let sut: FetchDeliveriesUseCase
 
 describe('Fetch Deliveries', () => {
   beforeEach(() => {
-    deliveriesRepository = new InMemoryDeliveriesRepository()
+    recipientsRepository = new InMemoryRecipientsRepository()
+    couriersRepository = new InMemoryCouriersRepository()
+    deliveriesRepository = new InMemoryDeliveriesRepository(
+      undefined,
+      recipientsRepository,
+      couriersRepository,
+    )
+
     sut = new FetchDeliveriesUseCase(deliveriesRepository)
   })
 
   it('should be able to fetch deliveries', async () => {
-    await deliveriesRepository.create(makeDelivery())
-    await deliveriesRepository.create(makeDelivery())
+    const recipient1 = makeRecipient()
+    await recipientsRepository.create(recipient1)
+    const recipient2 = makeRecipient()
+    await recipientsRepository.create(recipient2)
+
+    const courier1 = makeCourier()
+    await couriersRepository.create(courier1)
+
+    await deliveriesRepository.create(
+      makeDelivery({ recipientId: recipient1.id, courierId: courier1.id }),
+    )
+    await deliveriesRepository.create(
+      makeDelivery({ recipientId: recipient1.id }),
+    )
 
     const result = await sut.execute({
       page: 1,
@@ -27,27 +52,50 @@ describe('Fetch Deliveries', () => {
   })
 
   it('should be able to fetch deliveries by status', async () => {
+    const recipient1 = makeRecipient()
+    await recipientsRepository.create(recipient1)
+    const recipient2 = makeRecipient()
+    await recipientsRepository.create(recipient2)
+    const recipient3 = makeRecipient()
+    await recipientsRepository.create(recipient3)
+    const recipient4 = makeRecipient()
+    await recipientsRepository.create(recipient4)
+
+    const courier1 = makeCourier()
+    await couriersRepository.create(courier1)
+    const courier2 = makeCourier()
+    await couriersRepository.create(courier2)
+    const courier3 = makeCourier()
+    await couriersRepository.create(courier3)
+
     await deliveriesRepository.create(
       makeDelivery({
         status: DeliveryStatus.CREATED,
+        recipientId: recipient1.id,
       }),
     )
 
     await deliveriesRepository.create(
       makeDelivery({
         status: DeliveryStatus.IN_TRANSIT,
+        recipientId: recipient2.id,
+        courierId: courier1.id,
       }),
     )
 
     await deliveriesRepository.create(
       makeDelivery({
         status: DeliveryStatus.IN_TRANSIT,
+        recipientId: recipient3.id,
+        courierId: courier2.id,
       }),
     )
 
     await deliveriesRepository.create(
       makeDelivery({
         status: DeliveryStatus.COMPLETED,
+        recipientId: recipient4.id,
+        courierId: courier3.id,
       }),
     )
 
@@ -64,7 +112,14 @@ describe('Fetch Deliveries', () => {
 
   it('should be able to fetch paginated deliveries', async () => {
     for (let i = 0; i < 12; i++) {
-      await deliveriesRepository.create(makeDelivery())
+      const recipient = makeRecipient()
+      await recipientsRepository.create(recipient)
+
+      await deliveriesRepository.create(
+        makeDelivery({
+          recipientId: recipient.id,
+        }),
+      )
     }
 
     const result = await sut.execute({
