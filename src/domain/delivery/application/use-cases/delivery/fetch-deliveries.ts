da9dfc8ddd16b8ec9a0910a10c87a2aba7b91b62
Eduardo Zago <zagoe22@gmail.com>
2026-05-03 21:06:46 -0300

    refactor: update FetchDeliveriesUseCase to return DeliveryDetails read model
    Remove unused Delivery entity import and update return type from Delivery[] to DeliveryDetails[]. Add DeliveryDetails import from read-models. Aligns use case with CQRS pattern by returning read-optimized model for query operations

--- src/domain/delivery/application/use-cases/delivery/fetch-deliveries.ts
+++ src/domain/delivery/application/use-cases/delivery/fetch-deliveries.ts
@@ -1,10 +1,8 @@
 import { Either, right } from '@/core/either'
 import { Injectable } from '@nestjs/common'
 import { DeliveriesRepository } from '../../repositories/deliveries-repository'
-import {
-  Delivery,
-  DeliveryStatus,
-} from '@/domain/delivery/enterprise/entities/delivery'
+import { DeliveryStatus } from '@/domain/delivery/enterprise/entities/delivery'
+import { DeliveryDetails } from '../../repositories/read-models/delivery-details'
 
 interface FetchDeliveriesUseCaseRequest {
   status?: DeliveryStatus
@@ -15,7 +13,7 @@ interface FetchDeliveriesUseCaseRequest {
 export type FetchDeliveriesUseCaseResponse = Either<
   null,
   {
-    deliveries: Delivery[]
+    deliveries: DeliveryDetails[]
   }
 >
 
