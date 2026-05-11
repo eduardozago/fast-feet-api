282c08b0f56f175a710c08903ce85b3b8c61d0c4
Eduardo Zago <zagoe22@gmail.com>
2026-05-10 21:26:40 -0300

    test(delivery): add courier validation to PickUpDeliveryUseCase tests
    Add courier creation to test setup and courierId parameter to test cases. Add new test case to verify that pickup fails when wrong courier attempts to pick up delivery. Update existing tests to include courierId in execute calls and verify InvalidCourierAssignedError is thrown for invalid courier assignments

--- src/domain/delivery/application/use-cases/delivery/pick-up-delivery.spec.ts
+++ src/domain/delivery/application/use-cases/delivery/pick-up-delivery.spec.ts
@@ -3,6 +3,8 @@ import { makeDelivery } from 'test/factories/delivery/make-delivery'
 import { PickUpDeliveryUseCase } from './pick-up-delivery'
 import { DeliveryStatus } from '@/domain/delivery/enterprise/entities/delivery'
 import { CannotPickUpDeliveryError } from './errors/cannot-pick-up-error'
+import { makeCourier } from 'test/factories/delivery/make-courier'
+import { InvalidCourierAssignedError } from './errors/invalid-courier-assigned-error'
 
 let deliveriesRepository: InMemoryDeliveriesRepository
 let sut: PickUpDeliveryUseCase
@@ -14,14 +16,18 @@ describe('Pick Up Delivery', () => {
   })
 
   it('should be able to pick up delivery', async () => {
+    const courier = makeCourier()
+
     const delivery = makeDelivery({
       status: DeliveryStatus.ASSIGNED,
+      courierId: courier.id,
     })
 
     await deliveriesRepository.create(delivery)
 
     const result = await sut.execute({
       deliveryId: delivery.id.toString(),
+      courierId: courier.id.toString(),
     })
 
     expect(result.isRight()).toBe(true)
@@ -31,6 +37,8 @@ describe('Pick Up Delivery', () => {
   })
 
   it('should not be able to pick up delivery if delivery is not assigned', async () => {
+    const courier = makeCourier()
+
     const delivery = makeDelivery({
       status: DeliveryStatus.CREATED,
     })
@@ -39,10 +47,30 @@ describe('Pick Up Delivery', () => {
 
     const result = await sut.execute({
       deliveryId: delivery.id.toString(),
+      courierId: courier.id.toString(),
     })
 
     expect(result.isLeft()).toBe(true)
     expect(deliveriesRepository.items[0].status).toEqual(DeliveryStatus.CREATED)
     expect(result.value).toBeInstanceOf(CannotPickUpDeliveryError)
+  })
+
+  it('should not be able to pick up delivery with wrong courier', async () => {
+    const delivery = makeDelivery({
+      status: DeliveryStatus.ASSIGNED,
+    })
+
+    await deliveriesRepository.create(delivery)
+
+    const result = await sut.execute({
+      deliveryId: delivery.id.toString(),
+      courierId: 'different-courier-id',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(deliveriesRepository.items[0].status).toEqual(
+      DeliveryStatus.ASSIGNED,
+    )
+    expect(result.value).toBeInstanceOf(InvalidCourierAssignedError)
   })
 })
