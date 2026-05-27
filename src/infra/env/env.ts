b1cc555fe14ae5239c209410060c6bfe4cc04eec
Eduardo Zago <zagoe22@gmail.com>
2026-05-27 10:03:22 -0300

    chore(env): change default PORT from 3333 to 8080

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -4,7 +4,7 @@ export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   JWT_PRIVATE_KEY: z.string(),
   JWT_PUBLIC_KEY: z.string(),
-  PORT: z.coerce.number().optional().default(3333),
+  PORT: z.coerce.number().optional().default(8080),
   NOMINATIM_API_URL: z.string().url(),
   NOMINATIM_API_USER_AGENT: z.string(),
 })
